9227fd38d9d5c235bdd2c601571e8eb81245fed1
patrickbasham <PatrickTBasham@gmail.com>
2022-08-10 14:38:17 -0400

    set up styling for front -> back card transition

--- src/components/AboutCard/AboutCard.jsx
+++ src/components/AboutCard/AboutCard.jsx
@@ -1,17 +1,19 @@
 export const AboutCard = ({ id, img, name, detail, active, handleClick }) => {
     return (
-        <div className={`about-card ${active ? "active" : null}`} onClick={() => handleClick(id)}>
+        <div className={`about-card ${active ? "active" : null}`} onClick=
+            {() => handleClick(id)}>
+            <img src={img} alt="" />
             {active ?
                 <div className="about-card-back">
-                    {detail}
+                    <p className="description">
+                        {detail}
+                    </p>
                 </div>
                 :
-                <div className="about-card-name">
-                    <div className="about-card-front">
-                        <div className="title">
-                            <p>My</p>
-                            <p>{name}</p>
-                        </div>
+                <div className="about-card-front">
+                    <div className="title">
+                        <p>My</p>
+                        <p>{name}</p>
                     </div>
                 </div>
             }
