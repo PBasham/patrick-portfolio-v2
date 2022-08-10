d56d6098d3c34904ac76e44c5b4c7d18721903ef
patrickbasham <PatrickTBasham@gmail.com>
2022-08-10 02:07:55 -0400

    set up base for about page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,15 @@
 export const About = () => {
     return (
         <div className="section-about">
-            About
+            <div className="quote-div">
+                <div className="quote">
+                    <p>The electric light did not come from the continuous improvement of candles</p>
+                    <p className="text-">- Oren Harari</p>
+                </div>
+            </div>
+            <div className="cards-div">
+                cards cards cards
+            </div>
         </div>
     )
 }
