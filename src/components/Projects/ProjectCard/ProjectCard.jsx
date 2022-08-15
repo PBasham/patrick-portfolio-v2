d415ee3c8c0d847f29e0b069f38021352594a1ff
patrickbasham <PatrickTBasham@gmail.com>
2022-08-15 14:30:29 -0400

    updated spacing on projects section and text alignment on project detail and skills

--- src/components/Projects/ProjectCard/ProjectCard.jsx
+++ src/components/Projects/ProjectCard/ProjectCard.jsx
@@ -7,29 +7,31 @@ export const ProjectCard = ({ project, handleProjectClick }) => {
 
 
     return (
-        <LinkScroll
-            to="projects"
-            className="project-card"
-            onClick={() => handleProjectClick(project.id)}
-            smooth={true}
-            duration={500}
-        >
-            <div className="img-div">
-                <img src={project.img} alt="" />
-                <div className="title-shape-div">
-                    <div className="project-card-front">
-                        <p className="title">{project.title}</p>
+        <div className="project-card">
+
+            <LinkScroll
+                to="projects"
+                onClick={() => handleProjectClick(project.id)}
+                smooth={true}
+                duration={500}
+            >
+                <div className="img-div">
+                    <img src={project.img} alt="" />
+                    <div className="title-shape-div">
+                        <div className="project-card-front">
+                            <p className="title">{project.title}</p>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <ul className="technologies">
-                {project.technologies.map((tech, index) => (
-                    <li key={index} >
-                        {/* <div>{tech[0]}</div> */}
-                        <p>{tech[1]} /</p>
-                    </li>
-                ))}
-            </ul>
-        </LinkScroll>
+            </LinkScroll>
+                <ul className="technologies">
+                    {project.technologies.map((tech, index) => (
+                        <li key={index} >
+                            {/* <div>{tech[0]}</div> */}
+                            <p>{tech[1]} /</p>
+                        </li>
+                    ))}
+                </ul>
+        </div>
     )
 }
