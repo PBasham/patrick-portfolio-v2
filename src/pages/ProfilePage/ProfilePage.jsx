405b07ab33d2893b61a99b66d93014a3f0aa6496
patrickbasham <PatrickTBasham@gmail.com>
2022-10-25 11:59:56 -0400

    added Catch all path back to routes

--- src/pages/ProfilePage/ProfilePage.jsx
+++ src/pages/ProfilePage/ProfilePage.jsx
@@ -20,6 +20,7 @@ import {
     BrowserRouter as Router,
     Routes,
     Route,
+    Navigate
 } from "react-router-dom"
 
 
@@ -60,7 +61,7 @@ export const ProfilePage = () => {
                             <Route path="/projects" element={<Projects setNavLinks={setNavLinks} />} />
 
                             <Route path="/contact" element={<Contact setNavLinks={setNavLinks} />} />
-                            {/* <Route path="*" element={<Navigate to="/home" />} /> */}
+                            <Route path="*" element={<Navigate to="/home" />} />
                         </Routes>
                     </div>
                 </Router>
