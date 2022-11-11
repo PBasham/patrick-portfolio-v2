3766b04ac8b105f98f072ccba4e9f4bcef001b7a
patrickbasham <PatrickTBasham@gmail.com>
2022-11-11 11:35:32 -0500

    set up base for mobile nav menu

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,9 +3,14 @@
 ========================================*/
 import { Link } from "react-router-dom"
 import ReactGA from "react-ga"
+import { useState } from "react"
 
 export const NavBar = ({ navLinks }) => {
 
+    
+    const [navOpen, setNavOpen] = useState(false)
+    
+    
     const handleClickGA = (page) => {
 
         ReactGA.event({
@@ -14,24 +19,39 @@ export const NavBar = ({ navLinks }) => {
         })
     }
 
+    const handleNavChange = () => {
+        setNavOpen((current) => {
+            return (!current)
+            })
+    }
+    const handleNavClose = () => {
+        setNavOpen(false)
+    }
+
+
+
+
     return (
-        <div className="nav">
+        <div className={`nav ${navOpen ? `nav-open` : null}`}>
+            <div className={`hamburger-menu-div`}>
+                <div className="hamburger-menu" onClick={handleNavChange} ></div>
+            </div>
             <div className="nav-logo">
                 <img src="./img/logo.png" alt="" />
             </div>
             <ul className="nav-btns">
                 {navLinks.allNavLinks.map((link) => (
                     // <li
-                        // key={link.id}
+                    // key={link.id}
                     // >
-                        <Link
-                            key={link.id}
-                            to={link.to}
-                            className={`nav-btn ${link.id === navLinks.activeNavLink ? "active" : ""}`}
-                            onClick={() => handleClickGA(link.to)}
-                        >
-                            {link.name}
-                        </Link>
+                    <Link
+                        key={link.id}
+                        to={link.to}
+                        className={`nav-btn ${link.id === navLinks.activeNavLink ? "active" : ""}`}
+                        onClick={() => handleClickGA(link.to)}
+                    >
+                        {link.name}
+                    </Link>
                     // </li>
                 ))}
             </ul>
@@ -42,7 +62,9 @@ export const NavBar = ({ navLinks }) => {
                 <a id="link-linkedin" className="profile-link" href="https://www.linkedin.com/in/patrickbasham/" target="_blank">
                     <i className="fa fa-linkedin"></i>
                 </a>
-                <a className="resume" href="https://drive.google.com/file/d/11nqCIAy09AXARdXCuH09Wh1aIagw2YEU/view?usp=sharing" target={"_blank"}>CV</a>
+                <a className="resume" href="https://drive.google.com/file/d/11nqCIAy09AXARdXCuH09Wh1aIagw2YEU/view?usp=sharing" target={"_blank"}>
+                    <p>CV</p>
+                </a>
             </div>
         </div>
     )
